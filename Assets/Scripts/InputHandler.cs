d3345f98c0af1e469191a23447aea11eecddf7f5
Connor Checkley <connor.checkley@gmail.com>
2019-01-25 19:10:38 +0000

    Added input handler and player controller, added player prefab and default scene

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    public PlayerController playerController;
+
+    void Update()
+    {
+        HandleInput();
+    }
+
+    void HandleInput()
+    {
+        playerController.xAxisDelta = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            playerController.BeginJump();
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            playerController.BeginCrouch();
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            playerController.EndCrouch();
+        }
+    }
+}
