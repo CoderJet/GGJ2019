b79f6f18b5f9688ea7b0f0fbf902e331db64b5d1
nairu <Carniall@googlemail.com>
2019-01-26 12:59:41 +0000

    Checking in camera follow

---
+++ Assets/MouseMover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MouseMover : MonoBehaviour
+{
+    public GameObject setSelected;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EventSystem.current.SetSelectedGameObject(setSelected);
+    }
+}
