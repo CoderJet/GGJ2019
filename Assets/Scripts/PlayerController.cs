ee5e2fe3d0840cd0c1c57f9408d08e703d9eeec3
Connor Checkley <connor.checkley@gmail.com>
2019-01-26 02:28:54 +0000

    added gravity attraction and physics controls to player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,116 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-    [HideInInspector] public float xAxisDelta = 0f;
-    [HideInInspector] public bool isJumping = false;
-
-    [SerializeField] LayerMask groundLayer;
-    [SerializeField] Transform groundCheckTransform;
-    [SerializeField] float groundCheckRadius = 0f;
-
-    public float movementSpeed = 0f;
-    public float jumpForce = 0f;
-    public int maxJumps = 2;
-
-    int jumpCount;
-
-    bool isGrounded;
-    bool isFacingRight;
-
-    Animator animator;
     Rigidbody2D rigidbody2D;
+
+    public float movementSpeed;
+
+    Vector2 moveDirection;
 
     void Start()
     {
-        animator = GetComponent<Animator>();
         rigidbody2D = GetComponent<Rigidbody2D>();
-    }
-
-    void Awake()
-    {
-        jumpCount = 0;
-        isFacingRight = true;
-    }
-
-    void FixedUpdate()
-    {
-        bool wasGrounded = isGrounded;
-        isGrounded = IsGrounded();
-
-        if (!wasGrounded && isGrounded)
-        {
-            EndJump();
-        }
-
-        float horizontalMovementDelta = (xAxisDelta * movementSpeed) * Time.fixedDeltaTime;
-
-        Move(horizontalMovementDelta, isJumping);
-        isJumping = false;
     }
 
     void Update()
     {
-        float animMovementDelta = Mathf.Abs(xAxisDelta * movementSpeed);
-
-        animator.SetFloat("MovementSpeed", animMovementDelta);
+        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
     }
 
-    void Move(float movementDelta, bool isJumping)
+    void FixedUpdate()
     {
-        if (isJumping)
-        {
-            rigidbody2D.AddForce(new Vector2(0f, jumpForce));
-        }
-
-        if ((movementDelta > 0 && !isFacingRight) || (movementDelta < 0 && isFacingRight))
-        {
-            Flip();
-        }
-
-        rigidbody2D.velocity = new Vector2(movementDelta, rigidbody2D.velocity.y);
-    }
-
-    void Flip()
-    {
-        isFacingRight = !isFacingRight;
-
-        Vector3 currentScale = transform.localScale;
-        currentScale.x *= -1;
-        transform.localScale = currentScale;
-    }
-
-    bool IsGrounded()
-    {
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheckTransform.position, groundCheckRadius, groundLayer);
-
-        foreach (Collider2D collider in colliders)
-        {
-            if (collider.gameObject != gameObject)
-            {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    public void BeginJump()
-    {
-        if (jumpCount < maxJumps)
-        {
-            jumpCount++;
-            isJumping = true;
-            animator.SetBool("IsJumping", true);
-        }
-    }
-
-    public void EndJump()
-    {
-        jumpCount = 0;
-        isJumping = false;
-        animator.SetBool("IsJumping", false);
+        Vector2 transDirection = transform.TransformDirection(moveDirection) * movementSpeed * Time.deltaTime;
+        rigidbody2D.AddForce(transDirection);
     }
 }
