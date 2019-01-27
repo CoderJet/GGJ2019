4c144d5ed7a92a6279e8f509b524ce4d81ad7318
Joe Taylor <CoderJet@gmail.com>
2019-01-27 17:03:29 +0000

    Added base quest system, needs further work.

---
+++ Assets/Scripts/QuestData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data/Quest Details")]
+public class QuestData : ScriptableObject
+{
+    public string QuestName;
+    [Multiline]
+    public string Objectives;
+    public bool IsCompleted = false;
+    public QuestData NextQuest;
+}
---
+++ Assets/Scripts/QuestSystem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestSystem : MonoBehaviour
+{
+    [SerializeField] private QuestData currentQuest;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
